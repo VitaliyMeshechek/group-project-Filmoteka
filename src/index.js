c24b08252b30cbda302e7f0b9f6c4a63fbf13084
Vitaliy Meshechek <meshechek.vitaliy@ukr.net>
2022-12-25 14:08:04 +0200

    start

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './js/signIn/sing-in';
+import './js/home/home-movies';
+import './js/loader';
+import './js/modalMoviesCard';
+import './js/modal-group';
+import './js/pagination';
+import './js/trailer';
+import './js/home/search';
---
+++ src/library.js
@@ -0,0 +1,6 @@
+import './js/signIn/sing-in';
+import './js/library/library-header';
+import './js/loader';
+import './js/modalMoviesCard';
+import './js/modal-group';
+import './js/trailer';
